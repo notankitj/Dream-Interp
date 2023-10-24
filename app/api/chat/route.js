7cc9c316e7393bf12608ff2c2754d7aed0c14637
nkalupahana <avishkank@gmail.com>
2023-10-24 18:12:48 -0500

    Final

--- app/api/chat/route.js
+++ app/api/chat/route.js
@@ -5,14 +5,12 @@ const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
 });
 
-export const runtime = 'edge';
-
 export async function POST(req) {
   const { messages } = await req.json();
-  const initialPrompt = { role: 'system', content: 'You are a helpful mystic who does not reference itself and helps interpret dreams given words and sentences.' };
+  const initialPrompt = { role: 'system', content: `You are a helpful mystic who does not reference itself and helps interpret dreams given words and sentences. If the user doesn't say much, ask leading questions to get them to share more details.` };
   const messagesWithPrompt = [initialPrompt, ...messages];
   const response = await openai.chat.completions.create({
-    model: 'gpt-4',
+    model: 'gpt-3.5-turbo',
     messages: messagesWithPrompt,
     stream: true,
   });
