1c7c3258637789717e274ad45dfa951508e8ced0
nkalupahana <avishkank@gmail.com>
2023-10-24 18:14:26 -0500

    Create boilerplate

--- app/page.js
+++ app/page.js
@@ -1,28 +1,9 @@
 'use client';
 
-import { useChat } from 'ai/react';
-
 export default function Chat() {
-  const { messages, input, handleInputChange, handleSubmit } = useChat();
-
   return (
     <div className='chatContainer'>
-      <div className='messagesContainer'>
-        {messages.map(m => (
-          <div key={m.id} className={m.role === 'assistant' ? 'systemMessage' : 'userMessage'}>
-            <div className='messageContent'>{m.content}</div>
-          </div>
-        ))}
-      </div>
-
-      <form onSubmit={handleSubmit} className='inputForm'>
-        <input
-          value={input}
-          placeholder="What did you dream of..."
-          onChange={handleInputChange}
-          className='inputField'
-        />
-      </form>
+      <p>Put your code here!</p>
     </div>
   );
 }
