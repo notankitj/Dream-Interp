9f97597121ec6a2596d33618d9ac71c1e99ee335
notankitj <115118667+notankitj@users.noreply.github.com>
2023-10-28 22:17:49 -0500

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,9 +1,30 @@
-'use client';
+"use client";
+
+import { useChat } from "ai/react";
 
 export default function Chat() {
+  const { messages, input, handleInputChange, handleSubmit } = useChat();
+
   return (
-    <div className='chatContainer'>
-      <p>Put your code here!</p>
+    <div className="chatContainer">
+      <div className="messagesContainer">
+        {messages.map((m) => (
+          <div
+            key={m.id}
+            className={m.role === "assistant" ? "systemMessage" : "userMessage"}
+          >
+            <div className="messageContent">{m.content}</div>
+          </div>
+        ))}
+      </div>
+      <form onSubmit={handleSubmit} className="inputForm">
+        <input
+          value={input}
+          onChange={handleInputChange}
+          placeholder="What did you dream of..."
+          className="inputField"
+        />
+      </form>
     </div>
   );
 }
